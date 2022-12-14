2fa8b0bd9768e2376f54baea6f3a448883d0df46
Daniel Torres <danieltorres@outlook.pt>
2022-12-14 15:56:57 +0000

    Updated variable linting, code to convert first collumn to datetime and started working on the code to find non consequitive samples

--- DatasetAnalysis/readDataset.m
+++ DatasetAnalysis/readDataset.m
@@ -7,13 +7,13 @@ cd \\wsl.localhost\ubuntu\home\dtorres\Dissertation_NILM;
 clear, close all, clc;
 
 cd OriginalDataset/Equipment;
-fileList = dir;
-fileList = {fileList.name};
-fileList=fileList(3:end);   % remove . and ..
+file_list = dir;
+file_list = {file_list.name};
+file_list = file_list(3:end);   % remove . and ..
 
-n_Eq = size(fileList,2);
+n_Eq = size(file_list,2);
 for i=1:n_Eq
-    Eq_Data{i} = readtable(string(fileList(i)));
+    eq_data{i} = readtable(string(file_list(i)));
 end
 
 % 1 - 'doublepolecontactor-I.csv'	
@@ -36,36 +36,93 @@ end
 
 
 %% Total number of samples
-for i = 1 : size(Eq_Data,2)
-    aux = Eq_Data{i};
-    numbersSamples(i,:) = size(aux,1);
+clearvars -except eq_data
+for i = 1 : size(eq_data,2)
+    aux = eq_data{i};
+    numbers_samples(i,:) = size(aux,1);
 end
 
 %% Not unique samples, Nan samples
-numberSamples = [];
-uniqueSamples = [];
-notUniqueSamples = [];
-nanSamplesCounter = zeros(1,8);
-for i = 1 : size(Eq_Data,2)
-    arrayEq = [];
-    tableEq = Eq_Data{i};
-    numberSamples(i) = size(tableEq,1);
-    uniqueSamples(i) = size(unique(tableEq(:,1)),1);
-    notUniqueSamples(i) = numberSamples(i) - size(unique(tableEq(:,1)),1);
-    arrayEq = table2array(tableEq(:,2:end));
-    for j = 1 : numberSamples(i)
-        if ( (isnan(arrayEq(j,2)) + isnan(arrayEq(j,3)) + isnan(arrayEq(j,4)) + isnan(arrayEq(j,5))) > 0)
-                nanSamplesCounter(i) = nanSamplesCounter(i) + 1;
+clearvars -except eq_data
+number_samples = [];
+unique_samples = [];
+not_unique_samples = [];
+nan_samples_counter = zeros(1,8);
+for i = 1 : size(eq_data,2)
+    table_eq = eq_data{i};
+    number_samples(i) = size(table_eq,1);
+    unique_samples(i) = size(unique(table_eq(:,1)),1);
+    not_unique_samples(i) = number_samples(i) - size(unique(table_eq(:,1)),1);
+    array_eq = [];
+    array_eq = table2array(table_eq(:,2:end));
+    for j = 1 : number_samples(i)
+        if ( (isnan(array_eq(j,2)) + isnan(array_eq(j,3)) + isnan(array_eq(j,4)) + isnan(array_eq(j,5))) > 0)
+                nan_samples_counter(i) = nan_samples_counter(i) + 1;
         end
     end
 end
 
 % Starting and ending time samples
-arrayStart = {};
-arrayEnd = {};
-for i = 1 : size(Eq_Data,2)
-    tableEq = Eq_Data{i};
-    arrayStart(i) = table2array(tableEq(1,1));
-    arrayEnd(i) = table2array(tableEq(end,1));
+array_start = {};
+array_end = {};
+for i = 1 : size(eq_data,2)
+    tableEq = eq_data{i};
+    array_start(i) = table2array(table_eq(1,1));
+    array_end(i) = table2array(table_eq(end,1));
 end
 
+%% See commun timestamps between 8 equipment
+clearvars -except eq_data
+table_eq = eq_data{1};
+
+appended_tst = [];
+for i = 1 : size(eq_data,2)
+    table_eq = eq_data{i};
+    appended_tst = [appended_tst; categorical((table_eq(:,1).timestamp))];
+end
+
+a = categories(appended_tst);
+b = countcats(appended_tst);
+d = a(b == size(eq_data,2));
+
+%edges = unique(AppendedCellTimestamps,'stable');
+%b=cellfun(@(x) sum(ismember(AppendedCellTimestamps,x)),edges,'un',0);
+
+%% Missing non consecutive timestamps
+% Run previous code block
+clearvars -except eq_data
+
+aux = cell2mat(d);
+date_samples = datetime(aux(:,1:end-3));
+posix_date = posixtime(date_samples);
+observations = find(diff(posix_date) ~= 1) + 1;
+
+aux_date = d;
+aux_date(observations) = [];
+
+observations_datetime = find(diff(dateSamples) ~= seconds(1)) + seconds(1);
+% date = today("datetime")
+
+
+%%
+
+clearvars -except Eq_Data
+
+nanSamplesCounter = zeros(1,8);
+for i = 1 : size(Eq_Data,2)
+    tableEq = Eq_Data{i};
+    arrayEq = [];
+    
+    arrayEq = cell2mat(table2array(tableEq(:,1)));
+    arrayEq = arrayEq(j, 1:end-3);
+    for j = 1 : size(arrayEq,1)
+        timeEq(j,:) = convertCharsToStrings(arrayEq(j,:));
+    end
+
+    A() = convertCharsToStrings(arrayEq(j, 1:end-3));
+    A = cellfun(@calculateDatetime,arrayEq);
+end
+
+function datetimeX = calculateDatetime(x)
+    datetimeX = (convertCharsToStrings(x(1:end-3)));
+end
