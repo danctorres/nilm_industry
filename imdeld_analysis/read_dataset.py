b7538e070f306f8b15a078369c68ee7297d67f9a
Daniel Torres <danieltorres@outlook.pt>
2022-12-29 14:28:27 +0000

    mend

--- imdeld_analysis/read_dataset.py
+++ imdeld_analysis/read_dataset.py
@@ -1,20 +1,23 @@
+"""Module for reading csv and path functions."""
 import csv
 import os
 
 
-def read_file(file_path):
-    with open(file_path, 'r') as csv_file:
+def read_file(file_path: str) -> list[dict[str, str]] :
+    """Reads contents of csv file and save it to list of dictionaries"""
+    with open(file_path, 'r', encoding = "utf-8") as csv_file:
         lst = list(csv.DictReader(csv_file))
     return lst
 
-def main():
+def main() -> None:
+    """Testing"""
     path = 'imdeld_dataset/Equipment'
     os.chdir(f"{path}")
     for file in os.listdir():
         if file.endswith('.csv'):
             lst_dic_csv = read_file(f'{file}')
-            for x in range(len(lst_dic_csv)):
-                print (lst_dic_csv[x])
+            for index in enumerate(lst_dic_csv):
+                print (lst_dic_csv[index])
 
 if __name__ == "__main__":
     main()
