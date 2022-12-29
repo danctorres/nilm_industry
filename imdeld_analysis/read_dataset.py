160928c337cf0535c63b20105cf210f2b6315453
Daniel Torres <danieltorres@outlook.pt>
2022-12-29 14:05:50 +0000

    implemented python code to read the dataset

--- imdeld_analysis/read_dataset.py
+++ imdeld_analysis/read_dataset.py
@@ -2,23 +2,19 @@ import csv
 import os
 
 
-def read_text_file(file_path):
+def read_file(file_path):
     with open(file_path, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            var_key = row['key']
-            var_value = row['value']
-            data[var_key] = var_value
-        return data
+        lst = list(csv.DictReader(csv_file))
+    return lst
 
 def main():
     path = 'imdeld_dataset/Equipment'
     os.chdir(f"{path}")
-    print(os.getcwd())
     for file in os.listdir():
         if file.endswith('.csv'):
-            dic = read_text_file(f'{file}')
-            print(dic.keys())
+            lst_dic_csv = read_file(f'{file}')
+            for x in range(len(lst_dic_csv)):
+                print (lst_dic_csv[x])
 
 if __name__ == "__main__":
     main()
