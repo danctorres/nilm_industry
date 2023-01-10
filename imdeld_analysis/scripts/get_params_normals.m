83d9bf92468beaa0f3156bddd17097d7632df72d
Daniel Torres <danieltorres@outlook.pt>
2023-01-10 00:13:12 +0000

    added code to draw histogram of each state based on the peaks and middle of peaks

--- imdeld_analysis/scripts/get_params_normals.m
+++ imdeld_analysis/scripts/get_params_normals.m
@@ -1,4 +1,4 @@
-function [curves, params_normal, pd] = get_params_normals(size_eq, TF_cell, counts_cell, edges_cell, bin_center_cell)
+function [curves, params_normal, pd, group_power_limit] = get_params_normals(size_eq, TF_cell, counts_cell, edges_cell, bin_center_cell)
 % Objective: calculate the curves that fit the histogram, per peak, with the allfitdist() and
 % get the params of the normal function
 % Inputs: TF_cell (one if is peak), counts_cell (count of each edge of hist)
@@ -14,24 +14,13 @@ function [curves, params_normal, pd] = get_params_normals(size_eq, TF_cell, coun
         counts = counts_cell{j};
         bin_center_var = bin_center_cell{j};
         edges_var = edges_cell{j};
+        
         mid_peaks_index_interp = bin_center_var(mid_peaks_index);
 
 
         complete_edges = bin_center_var(1):bin_center_var(end);
         counts_interp = spline(bin_center_var, counts, complete_edges);
         
-        % Debug
-        plot(bin_center_var(1, 1:mid_peaks_index(1)), counts(1, 1:mid_peaks_index(1)), 'o')
-        
-        h = histogram(counts(1, 1:mid_peaks_index(1)), edges_var(1, 1:mid_peaks_index(1)));
-        hold on
-        pd = fitdist(counts(1, 1:mid_peaks_index(1))', 'Weibull');
-        xgrid = bin_center_var(1, 1:mid_peaks_index(1));
-        pdfEst = pdf(pd, xgrid);
-        line(xgrid, pdfEst)
-
-        %
-
 
         % figure,
         pd=[];
@@ -39,6 +28,8 @@ function [curves, params_normal, pd] = get_params_normals(size_eq, TF_cell, coun
             if (i == 1)
                 % Multivariate Copula Analysis Toolbox (MvCAT) - allfitdist()
                 curves(j, i) = {struct2table(allfitdist(counts(1, 1:mid_peaks_index(1))))};
+
+                group_power_limit(j, i) = {[1, bin_center_var(1, mid_peaks_index(1))]};
                 % curves(j, i) = {struct2table(allfitdist(counts_interp(1, 1:mid_peaks_index_interp(1))))};
                 % pd(j, i) = {fitdist(counts_interp(1, 1:mid_peaks_index(1))', 'Normal')};
                 
@@ -47,6 +38,7 @@ function [curves, params_normal, pd] = get_params_normals(size_eq, TF_cell, coun
             elseif (i == size(peaks_index, 2))
                 curves(j, i) = {struct2table(allfitdist(counts(1, mid_peaks_index(end):end)) )};
 
+                group_power_limit(j, i) = {[bin_center_var(1, mid_peaks_index(end)), bin_center_var(1, end)]};
                 % curves(j, i) = {struct2table(allfitdist(counts_interp(1, mid_peaks_index_interp(end):end)) )};
                 % pd(j, i) = {fitdist(counts_interp(1, mid_peaks_index(end):end)', 'Normal')};
                 % pd(j, i) = {fitdist([zeros(1, mid_peaks_index(end) - 1), counts_interp(1, mid_peaks_index(end):end)]', 'Normal')};
@@ -58,7 +50,8 @@ function [curves, params_normal, pd] = get_params_normals(size_eq, TF_cell, coun
                 curves(j, i) = {struct2table(allfitdist(counts(1, mid_peaks_index(i - 1):mid_peaks_index(i))))};
                 % curves(j, i) = {struct2table(allfitdist(counts_interp(1, mid_peaks_index_interp(i - 1):mid_peaks_index_interp(i))))};
                 % pd(j, i) = {fitdist(counts_interp(1, mid_peaks_index(i - 1):mid_peaks_index(i))', 'Normal')};
-                
+                group_power_limit(j, i) = {[bin_center_var(1, mid_peaks_index(i - 1)), bin_center_var(1, mid_peaks_index(i))]};
+
                 % subplot(size(peaks_index, 2), 1, i);
                 % histfit(counts_interp(1, mid_peaks_index(i - 1):mid_peaks_index(i)));
             end
@@ -76,4 +69,43 @@ function [curves, params_normal, pd] = get_params_normals(size_eq, TF_cell, coun
             end
         end
     end
+
+    
+    % Plot normals of each group by the value of the curves cell
+%     for j = 1:size_eq
+%         peaks_index = find(TF_cell{j} == 1);
+%         mid_peaks_index = zeros(1, size(peaks_index, 2) - 1);
+%         for i = 1:size(peaks_index, 2) - 1
+%             mid_peaks_index(i) = round((peaks_index(i + 1) + peaks_index(i)) / 2);
+%         end
+% 
+%         counts = counts_cell{j};
+%         for i = 1:size(peaks_index, 2)
+%             if (i == 1)
+%                 % Multivariate Copula Analysis Toolbox (MvCAT) - allfitdist()
+%                 counts_group(j, i) = {counts(1, 1:mid_peaks_index(1))};
+%             elseif (i == size(peaks_index, 2))
+%                 counts_group(j, i) = {counts(1, mid_peaks_index(end):end)};
+%             else
+%                 counts_group(j, i) = {counts(1, mid_peaks_index(i - 1):mid_peaks_index(i))};
+%             end
+%         end
+%     end  
+%     
+%     for i = 1:size(params_normal, 1)
+%         figure,
+%         sgtitle(sprintf('Equipment % i', i));
+%         for j = 1:size(params_normal, 2)
+%             aux = cell2mat(params_normal(i, j));
+%             if (~isempty(aux))
+%                 mu = aux(1, 1);
+%                 sigma = aux(1, 2);
+%                 x = -15*mu:15*mu;
+%                 y = normpdf(x,mu,sigma);
+%                 subplot(size(params_normal, 2), 1, j)
+%                 plot(x, y)
+%                 title(sprintf('State %i', j - 1));
+%             end
+%         end
+%     end
 end
--- imdeld_analysis/scripts/histogram_without_outliers.m
+++ imdeld_analysis/scripts/histogram_without_outliers.m
@@ -1,6 +1,7 @@
 function [output_counts_cell, output_edges_cell, output_bin_center, output_TF_cell] = histogram_without_outliers(varargin)
     % Objective: Create histograms of all the equipment active power samples for selected days
-    % Input: equipment_formated
+    % Input: equipment_formated, minimum_prominence (value for islocalmax
+    % function), true or false for removing outliers, save or not image
     % Output: histogram images
 
     equipment_formated = varargin{1};
@@ -12,8 +13,12 @@ function [output_counts_cell, output_edges_cell, output_bin_center, output_TF_ce
     figure('units', 'normalized', 'outerposition', [0, 0, 1, 1])
     for i = 1:size(equipment_formated, 2) - 1
         subplot((size(equipment_formated, 2) - 1) / 2, 2, i)
-        data        = table2array(equipment_formated(:, i + 1));
-        x_clean     = rmoutliers( data(data >= 0), 'mean', 'ThresholdFactor', 3);
+        data = table2array(equipment_formated(:, i + 1));
+        if varargin{3} == true
+            x_clean = rmoutliers( data(data >= 0), 'mean', 'ThresholdFactor', 3);
+        else
+            x_clean = data;
+        end
 
         [counts, edges] = histcounts(x_clean);
         histHandle      = histogram('BinEdges', edges, 'BinCounts', counts);
@@ -21,8 +26,8 @@ function [output_counts_cell, output_edges_cell, output_bin_center, output_TF_ce
         bin_center      = bin_center(1:end - 1);
         hold on
 
-        % Find the peaks in the histogram        
-        [TF, ~] = islocalmax([0, counts], 'MinProminence', 2000);
+        % minimum_prominence = varargin{2}
+        [TF, ~] = islocalmax([0, counts], 'MinProminence', varargin{2});
 
         output_counts_cell(i)   = {counts};
         output_edges_cell(i)    = {edges};
@@ -30,9 +35,6 @@ function [output_counts_cell, output_edges_cell, output_bin_center, output_TF_ce
         output_TF_cell(i)       = {TF(2:end)};
 
         plot(bin_center, counts, bin_center(TF(2:end)), counts(TF(2:end)),'r*')
-
-        [TF, ~] = islocalmax([0, counts], 'MinProminence', 2000);
-
 
         title(sprintf('Equipment %i', i))
         xlabel('Power [W]')
@@ -43,7 +45,7 @@ function [output_counts_cell, output_edges_cell, output_bin_center, output_TF_ce
         hold off
     end
     
-    if (nargin == 2 && varargin{2} == true)
+    if (nargin == 3 && varargin{3} == true)
         file_information = matlab.desktop.editor.getActive;
         [~, file_name, file_ext] = fileparts(file_information.Filename);
         saveas(gcf, [erase(file_information.Filename, ['\scripts\', file_name, file_ext]), '\results\images\histogram_equipment_no_outliers.fig']);
