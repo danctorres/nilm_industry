bcd75140b421afe04a17cba63babbc61ea40ad5b
Daniel Torres <danieltorres@outlook.pt>
2023-01-14 23:47:51 +0000

    added array of equipment index as input in function find_common_timestamps

--- imdeld_analysis/scripts/find_common_timestamps.m
+++ imdeld_analysis/scripts/find_common_timestamps.m
@@ -1,11 +1,12 @@
-function [common_timestamps] = find_common_timestamps(equip_data)
+function [common_timestamps] = find_common_timestamps(equip_data, eq_index)
     % Objective: Find common timestamps between 8 equipment
-    % Input: equip_data (cell array with equipment samples)
+    % Input: equip_data (cell array with equipment samples), eq_index (array with the index of the equipment to consider)
     % Output: common_timestamps (datetime sorted arrray with the dates of the common timestamp between the equipment)
 
     all_dates = [];
-    for i = 1:size(equip_data, 2)
-        table_eq = equip_data{i};
+
+    for i = 1:size(eq_index, 2)
+        table_eq = equip_data{eq_index(i)};
         datetime_values = cell2mat(unique(table_eq.timestamp));
         all_dates = cat(1, all_dates, datetime(datetime_values(:, 1:end - 3)));
     end
@@ -29,5 +30,3 @@ function [common_timestamps] = find_common_timestamps(equip_data)
     % aux3 = find(date_samples == aux2);
     % size(aux3, 1) == max(N)
 end
-
-
