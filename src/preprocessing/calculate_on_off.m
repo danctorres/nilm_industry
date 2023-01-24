d02e0bb5d2afe897c082c1f4a5b4367dbfe4cd7c
Daniel Torres <danieltorres@outlook.pt>
2023-01-24 19:23:38 +0000

    function to save training and validation data corrected

--- src/preprocessing/calculate_on_off.m
+++ src/preprocessing/calculate_on_off.m
@@ -26,8 +26,8 @@ function [on_off_array] = calculate_on_off(data, group_power_limit, save)
 
     if save == true
         file_information = matlab.desktop.editor.getActive;
-        [~, called_file_name, ~] = fileparts(file_information.Filename);
-        writetable(units_formated, join([erase(file_information.Filename,  join(['\src\preprocessing\', called_file_name, '.m'])), 'data\interim\on_off.csv'], '\'));
+        [~, file_name, file_ext] = fileparts(file_information.Filename);
+        writetable(units_formated, join([erase(file_information.Filename,  join(['\src\preprocessing\', file_name, '.m'])), '\data\interim\on_off.csv'], '\'));
         saveas(gcf, join([erase(file_information.Filename, ['\src\preprocessing\', file_name, file_ext]), '\reports\figures\on_off.fig'], '') );
         saveas(gcf, join([erase(file_information.Filename, ['\src\preprocessing\', file_name, file_ext]), '\reports\figures\on_off.png'], '') );
     end
--- src/preprocessing/save_final_data.m
+++ src/preprocessing/save_final_data.m
@@ -6,20 +6,20 @@ function [aggregate_training, aggregate_validation, on_off_training, on_off_vali
     % Divide into 70% training, 30% testing and equipment power consumption for validation
     size_training = round(size(aggregate, 1) * percentage_train);
     
-    aggregate_training = aggregate(1:size_training, :);
-    aggregate_validation = aggregate(size_training:end, :);
-    on_off_training = on_off(1:size_training, :);
-    on_off_validation = on_off(size_training:end, :);
+    aggregate_training      = aggregate(1:size_training, :);
+    aggregate_validation    = aggregate(size_training:end, :);
+    on_off_training         = on_off(1:size_training, :);
+    on_off_validation       = on_off(size_training:end, :);
 
-    equipment_validation = equipment_active_pow(size_training:end, :);
+    equipment_validation    = equipment_active_pow(size_training:end, :);
 
     if save == true
-        file_information = matlab.desktop.editor.getActive;
-        [~, called_file_name, ~] = fileparts(file_information.Filename);
-        writetable(units_formated, join([erase(file_information.Filename,  join(['\src\preprocessing\', called_file_name, '.m'])), 'data\processed\aggregate_training.csv'], '\'));
-        writetable(units_formated, join([erase(file_information.Filename,  join(['\src\preprocessing\', called_file_name, '.m'])), 'data\processed\aggregate_validation.csv'], '\'));
-        writetable(units_formated, join([erase(file_information.Filename,  join(['\src\preprocessing\', called_file_name, '.m'])), 'data\processed\on_off_training.csv'], '\'));
-        writetable(units_formated, join([erase(file_information.Filename,  join(['\src\preprocessing\', called_file_name, '.m'])), 'data\processed\on_off_validation.csv'], '\'));
-        writetable(units_formated, join([erase(file_information.Filename,  join(['\src\preprocessing\', called_file_name, '.m'])), 'data\processed\equipment_validation.csv'], '\'));
+        file_information    = matlab.desktop.editor.getActive;
+        [~, file_name, ~] = fileparts(file_information.Filename);
+        writetable(aggregate_training, join([erase(file_information.Filename,  join(['\src\preprocessing\', file_name, '.m'])), '\data\processed\aggregate_training.csv'], '\'));
+        writetable(aggregate_validation, join([erase(file_information.Filename,  join(['\src\preprocessing\', file_name, '.m'])), '\data\processed\aggregate_validation.csv'], '\'));
+        writematrix(on_off_training, join([erase(file_information.Filename,  join(['\src\preprocessing\', file_name, '.m'])), '\data\processed\on_off_training.csv'], '\'))
+        writematrix(on_off_validation, join([erase(file_information.Filename,  join(['\src\preprocessing\', file_name, '.m'])), '\data\processed\on_off_validation.csv'], '\'))
+        writetable(equipment_validation, join([erase(file_information.Filename,  join(['\src\preprocessing\', file_name, '.m'])), '\data\processed\equipment_validation.csv'], '\'));
     end
 end
