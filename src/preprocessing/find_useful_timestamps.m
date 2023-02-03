4d6fea5cea519a722b19f5b7fd6c6cdc60667aca
Daniel Torres <danieltorres@outlook.pt>
2023-02-03 23:58:04 +0000

    Added option to considered the interval between samples

--- src/preprocessing/find_useful_timestamps.m
+++ src/preprocessing/find_useful_timestamps.m
@@ -5,17 +5,28 @@ function [useful_common_timestamps] = find_useful_timestamps(common_timestamps)
 
 
     dates_only = datetime(datestr(common_timestamps, 'dd-mmm-yyyy'));
-    unique_dates = unique(dates_only);
+    [unique_dates, ~, indices] = unique(dates_only);
+
+    % Calculate max difference between samples
+    start_indices = [1; find(diff(indices)) + 1];
+    end_indices = [find(diff(indices)); size(dates_only, 1)];
+    max_diff = zeros(size(unique_dates, 1), 1);
+    for i = 1:size(unique_dates, 1)
+        max_diff(i, 1) = seconds(max(diff(common_timestamps(start_indices(i):end_indices(i)))));
+    end
+
     counts = histcounts(dates_only', [unique_dates', max(unique_dates) + 1]);
+
     % create table with the unique days, and number of samples
-    summary = table(unique_dates(:), counts(:), 'VariableNames', {'Date', 'count'});
+    summary = table(unique_dates(:), counts(:), max_diff(:), 'VariableNames', {'date', 'count', 'max_diff'});
     
-    % find days with more than 84600 samples % 83520
-    threshold_number_samples = 83520;
-    days_with_more_samples = summary(summary.count > threshold_number_samples, :);
+    % find days with more than 84600 samples 83520
+    threshold_total_missing = 2880;
+    threshold_interval = 15;
+    days_with_more_samples = summary(summary.count > (86400 - threshold_total_missing) & (summary.max_diff < threshold_interval), :);
     
     % get the common timestamps for the days_with_more_samples
-    [~, idx] = ismember(dates_only, days_with_more_samples.Date);
+    [~, idx] = ismember(dates_only, days_with_more_samples.date);
     
     index = [];
     for i = 1:size(days_with_more_samples, 1)
