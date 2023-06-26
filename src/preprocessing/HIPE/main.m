658613caaef300c4432a8bb4a27a4c0881ecb4d7
Daniel Torres <danieltorres@outlook.pt>
2023-06-26 03:07:51 +0100

    Add script to preprocessed the 1 week HIPE data

--- src/preprocessing/HIPE/main.m
+++ src/preprocessing/HIPE/main.m
@@ -1,5 +1,9 @@
+%% ----------------------- DATASET PRE-PROCESSING ---------------------- %%
+% Created by Daniel Torres
+
 close all; clear; clc;
 
+%% Read data
 % Set path
 file_information = matlab.desktop.editor.getActive;
 [file_dir, ~, ~] = fileparts(file_information.Filename);
@@ -12,57 +16,111 @@ agg_data = read_aggregate('HIPE');
 clearvars file_dir file_information
 
 
-
+%% Processed equipment data
 numRows = ceil(sqrt(size(equip_data, 2)));
 numCols = ceil(size(equip_data, 2) / 4);
 
-
 figure,
-clear equipment_tab;
+clear eq_cell;
 for i = 1 : size(equip_data, 2)
-   equipment_tab{i} = equip_data{i}(:, {'SensorDateTime', 'P_kW'});
-   columnData = equipment_tab{i}.P_kW;
+   eq_cell{i} = equip_data{i}(:, {'SensorDateTime', 'P_kW'});
    subplot(numRows, numCols, i);
-   plot(columnData);
+   plot(eq_cell{i}.P_kW);
 end
 
-equipment_processed_tab = {};
+eq_processed_timetable = {};
 figure,
 for i = 1 : size(equip_data, 2)
-    myTable1 = equipment_tab{i};
-    myTable1.SensorDateTime = cellfun(@(x) x(1, 1:22), myTable1.SensorDateTime, 'UniformOutput', false);
-    myTable1.SensorDateTime = strrep(myTable1.SensorDateTime, '+', '.');
-    myTable1.SensorDateTime = datetime(myTable1.SensorDateTime, 'InputFormat',"yyyy-MM-dd'T'HH:mm:ss.SS");
+    eq_buff = eq_cell{i};
+    eq_buff.Properties.VariableNames{'SensorDateTime'} = 'Time';
+    eq_buff.Time = cellfun(@(x) x(1, 1:22), eq_buff.Time, 'UniformOutput', false);
+    eq_buff.Time = strrep(eq_buff.Time, '+', '.');
+    eq_buff.Time = datetime(eq_buff.Time, 'InputFormat',"yyyy-MM-dd'T'HH:mm:ss.SS");
     
-    stopIndex = find(diff(myTable1.SensorDateTime) <= 0, 1);
+    stopIndex = find(diff(eq_buff.Time) <= 0, 1);
     if stopIndex > 0
-        myTable1(stopIndex : end, :) = [];
+        eq_buff(stopIndex : end, :) = [];
     end
-    clear stopIndex;
 
-    TT1 = timetable(myTable1.SensorDateTime, myTable1.P_kW);
-    newTimeVector = myTable1.SensorDateTime(1):seconds(1):myTable1.SensorDateTime(end);
-    newTable = retime(TT1, newTimeVector, 'linear');
-    clear newTimeVector;
-
-    equipment_processed_tab{i} = newTable;
-    subplot(numRows, numCols, i);
-    plot(newTable.Var1)
-    clear newTable;
+    eq_processed_timetable{i} = retime(timetable(eq_buff.Time, eq_buff.P_kW), eq_buff.Time(1):seconds(1):eq_buff.Time(end), 'linear');
+    clear stopIndex eq_buff;
 end
 
-aux1 = equipment_processed_tab{1};
-synchronizedTable = synchronize(equipment_processed_tab{:},'union','linear');
+eq_processed_table = timetable2table(synchronize(eq_processed_timetable{:}, 'union', 'spline'));
+eq_processed_table.Properties.VariableNames{'Var1_1'} = 'Eq1_P_kW';
+eq_processed_table.Properties.VariableNames{'Var1_2'} = 'Eq2_P_kW';
+eq_processed_table.Properties.VariableNames{'Var1_3'} = 'Eq3_P_kW';
+eq_processed_table.Properties.VariableNames{'Var1_4'} = 'Eq4_P_kW';
+eq_processed_table.Properties.VariableNames{'Var1_5'} = 'Eq5_P_kW';
+eq_processed_table.Properties.VariableNames{'Var1_6'} = 'Eq6_P_kW';
+eq_processed_table.Properties.VariableNames{'Var1_7'} = 'Eq7_P_kW';
+eq_processed_table.Properties.VariableNames{'Var1_8'} = 'Eq8_P_kW';
+eq_processed_table.Properties.VariableNames{'Var1_9'} = 'Eq9_P_kW';
+eq_processed_table.Properties.VariableNames{'Var1_10'} = 'Eq10_P_kW';
+clear equip_data eq_processed_timetable eq_cell;
 
-
-ON_OFF = timetable2table(synchronizedTable);
-ON_OFF = table2array(ON_OFF(:, 2:end));
-ON_OFF(ON_OFF > 0) = 1;
-ON_OFF(ON_OFF < 0) = 0;
+ON_OFF_double = table2array(eq_processed_table(:, 2:end));
+ON_OFF_double(ON_OFF_double > 0) = 1;
+ON_OFF_double(ON_OFF_double < 0) = 0;
 
 figure,
-for i = 1 : size(ON_OFF, 2)
+for i = 1 : size(ON_OFF_table, 2)
     subplot(numRows, numCols, i);
-    plot(ON_OFF(:, i));
+    plot(ON_OFF_double(:, i));
 end
 
+column_names = string(eq_processed_table.Properties.VariableNames);
+ON_OFF_table = array2table(ON_OFF_double, 'VariableNames', column_names(2:end));
+ON_OFF_table = addvars(ON_OFF_table, eq_processed_table.Time, 'Before', 1, 'NewVariableNames', 'Time');
+
+clear  numCols numRows;
+
+
+
+%% Processed aggregate data
+agg_power_data = agg_data(:, {'SensorDateTime', 'P_kW'});
+agg_power_data.SensorDateTime = cellfun(@(x) x(1, 1:22), agg_power_data.SensorDateTime, 'UniformOutput', false);
+agg_power_data.SensorDateTime = strrep(agg_power_data.SensorDateTime, '+', '.');
+agg_power_data.SensorDateTime = datetime(agg_power_data.SensorDateTime, 'InputFormat',"yyyy-MM-dd'T'HH:mm:ss.SS");
+
+stopIndex = find(diff(agg_power_data.SensorDateTime) <= 0, 1);
+if stopIndex > 0
+    agg_power_data(stopIndex : end, :) = [];
+end
+clear stopIndex;
+agg_interpolated_table = retime(timetable(agg_power_data.SensorDateTime, agg_power_data.P_kW), agg_power_data.SensorDateTime(1):seconds(1):agg_power_data.SensorDateTime(end), 'linear');
+
+aggregate_table = timetable2table(synchronize(timetable(eq_processed_table.Time, eq_processed_table.Eq1_P_kW), agg_interpolated_table, 'union', 'spline'));
+aggregate_table = removevars(aggregate_table, "Var1_1");
+aggregate_table.Properties.VariableNames{'Var1_agg_interpolated_table'} = 'Agg_P_kW';
+
+
+
+%% Split into training and validation data
+trainingRatio = 0.8;
+validationRatio = 1 - trainingRatio;
+
+partition = cvpartition(size(eq_processed_table, 1), 'HoldOut', validationRatio);
+trainingIndices = training(partition);
+validationIndices = test(partition);
+
+equipment_training = eq_processed_table(trainingIndices, :);
+equipment_validation = eq_processed_table(validationIndices, :);
+
+on_off_training = ON_OFF_table(trainingIndices, :);
+on_off_validation = ON_OFF_table(validationIndices, :);
+
+aggregate_training = aggregate_table(trainingIndices, :);
+aggregate_validation = aggregate_table(validationIndices, :);
+
+
+%% Save data
+
+relativeFolderPath = '../../../data/processed/HIPE/1_week/';
+
+writetable(equipment_training, fullfile(relativeFolderPath, 'equipment_training.csv'));
+writetable(equipment_validation, fullfile(relativeFolderPath, 'equipment_validation.csv'));
+writetable(on_off_training, fullfile(relativeFolderPath, 'on_off_training.csv'));
+writetable(on_off_validation, fullfile(relativeFolderPath, 'on_off_validation.csv'));
+writetable(aggregate_training, fullfile(relativeFolderPath, 'aggregate_training.csv'));
+writetable(aggregate_validation, fullfile(relativeFolderPath, 'aggregate_validation.csv'));
